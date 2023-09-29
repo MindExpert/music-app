a91a3bb6370bc5415e4f76954e39157ff8cbb67d
e.muchasaj <e.muchasaj@7btc.co>
2023-09-29 17:56:54 +0200

    added son items and query firebase

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -4,7 +4,7 @@
         <div class="md:grid md:grid-cols-3 md:gap-4">
             <div class="col-span-1">
                 <app-upload ref="upload"/>
-            </div>
+            </div> 
             <div class="col-span-2">
                 <div class="bg-white rounded border border-gray-200 relative flex flex-col">
                     <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
@@ -13,83 +13,7 @@
                     </div>
                     <div class="p-6">
                         <!-- Composition Items -->
-                        <div class="border border-gray-200 p-3 mb-4 rounded">
-                            <div>
-                                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                                    <i class="fa fa-times"></i>
-                                </button>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                                    <i class="fa fa-pencil-alt"></i>
-                                </button>
-                            </div>
-                            <div>
-                                <form>
-                                    <div class="mb-3">
-                                        <label class="inline-block mb-2">Song Title</label>
-                                        <input type="text"
-                                            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                            placeholder="Enter Song Title" />
-                                    </div>
-                                    <div class="mb-3">
-                                        <label class="inline-block mb-2">Genre</label>
-                                        <input type="text"
-                                            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                                            placeholder="Enter Genre" />
-                                    </div>
-                                    <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">
-                                        Submit
-                                    </button>
-                                    <button type="button" class="py-1.5 px-3 rounded text-white bg-gray-600">
-                                        Go Back
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="border border-gray-200 p-3 mb-4 rounded">
-                            <div>
-                                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                                    <i class="fa fa-times"></i>
-                                </button>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                                    <i class="fa fa-pencil-alt"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <div class="border border-gray-200 p-3 mb-4 rounded">
-                            <div>
-                                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                                    <i class="fa fa-times"></i>
-                                </button>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                                    <i class="fa fa-pencil-alt"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <div class="border border-gray-200 p-3 mb-4 rounded">
-                            <div>
-                                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                                    <i class="fa fa-times"></i>
-                                </button>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                                    <i class="fa fa-pencil-alt"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <div class="border border-gray-200 p-3 mb-4 rounded">
-                            <div>
-                                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                                    <i class="fa fa-times"></i>
-                                </button>
-                                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                                    <i class="fa fa-pencil-alt"></i>
-                                </button>
-                            </div>
-                        </div>
+                        <composition-item v-for="song in songs" :key="song.docId" :song="song"/>
                     </div>
                 </div>
             </div>
@@ -99,13 +23,35 @@
   
 <script>
 import AppUpload from '../components/Upload.vue';
-//import useUserStore from "@/stores/user";
+import CompositionItem from '../components/CompositionItem.vue';
+import { songsCollection, auth } from "@/includes/firebase";
 
 export default {
     name: "Manage",
     components: {
         AppUpload,
+        CompositionItem,
     },
+    data() {
+        return {
+            songs: []
+        }
+    },
+    async created() {
+        const userId = auth.currentUser.uid;
+
+        const querySnapshot = await songsCollection.where("uid", "==", userId).get();
+
+        querySnapshot.forEach((document) => {
+            console.log(document.id, " => ", document.data());
+            const song = {
+                docId: document.id,
+                ...document.data()
+            };
+
+            this.songs.push(song);
+        });
+    }
     //beforeRouteEnter(to, from, next) {
     //   const store = useUserStore();
     //   if (store.userLoggedIn) {
