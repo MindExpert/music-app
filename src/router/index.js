2e1333f97575e2eb3368940b9f16c23adaa13049
e.muchasaj <e.muchasaj@gmail.com>
2023-10-02 23:23:11 +0200

    dynamic routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,69 +2,75 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
 import Manage from "@/views/Manage.vue";
+import Song from "@/views/Song.vue";
 import useUserStore from "@/stores/user";
 
 const routes = [
-  {
-    name: "home",
-    path: "/",
-    component: Home,
-  },
-  {
-    name: 'about',
-    path: '/about',
-    component: About,
-    // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import('@/views/About.vue'),
-  },
-  {
-    name: "manage",
-    // alias: "/manage",
-    path: "/manage-music",
-    component: Manage,
-    beforeEnter(to, from, next) {
-      console.log("Manage Route Guard");
-      next();
+    {
+        name: "home",
+        path: "/",
+        component: Home,
     },
-    // Meta fields on the routes. We'll use it if route requires authentication
-    meta: {
-      requiresAuth: true,
+    {
+        name: "about",
+        path: "/about",
+        component: About,
+        // route level code-splitting
+        // this generates a separate chunk (About.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        // component: () => import('@/views/About.vue'),
     },
-  },
-  {
-    path: "/manage",
-    redirect: { name: "manage" },
-  },
-  {
-    path: "/:catchAll(.*)*",
-    redirect: { name: "home" },
-  },
+    {
+        name: "manage",
+        // alias: "/manage",
+        path: "/manage-music",
+        component: Manage,
+        beforeEnter(to, from, next) {
+            console.log("Manage Route Guard");
+            next();
+        },
+        // Meta fields on the routes. We'll use it if route requires authentication
+        meta: {
+            requiresAuth: true,
+        },
+    },
+    {
+        path: "/manage",
+        redirect: { name: "manage" },
+    },
+    {
+        name: "song",
+        path: "/song/:id",
+        component: Song,
+    },
+    {
+        path: "/:catchAll(.*)*",
+        redirect: { name: "home" },
+    },
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-  linkExactActiveClass: "text-yellow-500",
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+    linkExactActiveClass: "text-yellow-500",
 });
 
 /***  ROUTE GARDS ***/
 router.beforeEach((to, from, next) => {
-  // console.log("Global Guard");
+    // console.log("Global Guard");
 
-  if (!to.meta.requiresAuth) {
-    next();
-    return;
-  }
+    if (!to.meta.requiresAuth) {
+        next();
+        return;
+    }
 
-  const store = useUserStore();
+    const store = useUserStore();
 
-  if (store.userLoggedIn) {
-    next();
-  } else {
-    next({ name: "home" });
-  }
+    if (store.userLoggedIn) {
+        next();
+    } else {
+        next({ name: "home" });
+    }
 });
 
 export default router;
---
+++ src/views/Song.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        {{ $route.params.id }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Song",
+    data() {
+        return {
+            song: {},
+        };
+    },
+    // created() {
+    //     this.fetchData();
+    // },
+    // methods: {
+    //     fetchData() {
+    //         const id = this.$route.params.id;
+    //         this.$http.get(`/api/songs/${id}`).then((response) => {
+    //             this.song = response.data;
+    //         });
+    //     },
+    // },
+};
+</script>
